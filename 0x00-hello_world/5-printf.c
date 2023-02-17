73df92aac5fbcec6b206c574a868efcf8f7565be
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-17 09:39:08 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using printf to print the given word
+ *
+ * return:0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
