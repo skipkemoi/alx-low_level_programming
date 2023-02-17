210b2ed13e4756c29b765ffccbfc35ea5d5b2299
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-17 09:03:51 -0800

    c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the program prints programming is like building multilingual puzzle
+ * main - return value : 0 (success)
+ */
+	int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+/* return: 0 (success)*/
+return (0);
+}
