ef52882a32c0853afda71db00350db20670a072e
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-23 11:41:52 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - checks the absolute value
+ * @t: the interger to be computed
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
