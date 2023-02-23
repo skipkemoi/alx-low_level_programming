011dfcae0208aaf35930afe3b66ee289ff4db3d9
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-23 10:50:01 -0800

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 
 	char letter;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
