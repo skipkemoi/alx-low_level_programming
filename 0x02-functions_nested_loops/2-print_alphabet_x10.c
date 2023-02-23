a15239f36186731dceb509df42e3b307f12f32ae
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-23 10:47:37 -0800

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+
+	char letter;
+
+	for (num = 0; num < 9; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		_putchar('\n');
+	}
+}
