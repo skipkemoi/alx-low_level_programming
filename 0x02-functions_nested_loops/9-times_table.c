a317991444ad1122bd624e435ebc5cfaeb3a9df9
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-25 08:43:23 -0800

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 1; mult  <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			prod = num * mult;
-			if (prod <=0)
+			if (prod <= 0)
 				_putchar(' ');
 			else
 			{
