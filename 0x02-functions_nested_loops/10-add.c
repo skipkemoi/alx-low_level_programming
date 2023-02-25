ae731f7195046741d188e2a387133da046e380cb
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-25 09:01:00 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two intergers and provide results
+ * @a: the first number
+ * @b: the second number
+ * Return: the addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
