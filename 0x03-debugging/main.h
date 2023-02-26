a017e9744cc34f2046533ade19b250f04e35cef8
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-02-26 10:57:11 -0800

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include <stdio>
+void postitive_or_negative(int i)
+int largest_number(int a, int b, int c)
+void print_remaining_days(int day, int month, int year)
+int convert_day(int day, int month)
+#endif	
