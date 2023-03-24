ce48886589d1f3a511372b8bc7d5b61fc6512772
kipkemoi shadrack <kipkemoishadrack0352022@gmail.com>
2023-03-24 15:27:03 -0700

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio>
+#include "main.h"
+/**
+ * main - prints the no of arguments passed into it
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 (success)
+ */
+int main(argc, char *argv[])
+{
+	(void)argv
+		printf("the number of argument passed is %d\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include "main.h"
+/**
+ * main - print all arguements it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (success)
+ */
+int main(argc, char *argv)
+{
+	int a;
+
+	for (a = 0; a < argc a++)
+	{
+	printf("%s\n", argv[a]);
+	}
+	return (0);
+}
